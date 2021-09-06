e5edd7ae5dcd877b8d59bc0cbfc343d5f91fe064
hant82 <hant82@gmail.com>
2021-09-06 09:43:21 +0700

    code html

---
+++ Homework/learnhtml/main.py
@@ -0,0 +1,13 @@
+
+print ("Mời bạn nhập vào nhiệt độ tính theo độ C:")
+f_nhap= input()
+f_nhap = float (f_nhap)
+f_f=  float(f_nhap*(9/5)+32)
+f_k= float (f_nhap+273.15)
+f_r= float ((f_nhap+237.15)*(9/5))
+
+print ("Độ F tương ứng là:", f_f)
+print ("Độ K tương ứng là:", f_k)
+print ("Độ R tương ứng là:", f_r)
+
+ascii 
